f188cf5c60a3a7ff68d805bf09346a3a4cc28ed8
Liam Boreback <boreback.work@gmail.com>
2026-02-15 17:13:10 +0100

    Make vimtex build to a separate build folder

--- nvim/lua/packages.lua
+++ nvim/lua/packages.lua
@@ -203,6 +203,7 @@ require('lazy').setup({
     init = function()
       -- VimTeX configuration goes here, e.g.
       vim.g.vimtex_view_method = "zathura"
+      vim.g.vimtex_compiler_latexmk = { out_dir = "build" }
     end
   },
 
